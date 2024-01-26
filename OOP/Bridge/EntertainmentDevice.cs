55bec47b48f79b83405944171a00df8c69844351
Bojan Drezgic <nesquick7777@gmail.com>
2024-01-26 12:56:21 +0100

    Added abstract class EntertainmentDevice

--- OOP/Bridge/EntertainmentDevice.cs
+++ OOP/Bridge/EntertainmentDevice.cs
@@ -1,13 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bridge
 {
-    internal class EntertainmentDevice
+    internal abstract class EntertainmentDevice
     {
+        public int deviceState;
+
+        public int maxSetting;
+
+        public int volumeLevel = 0;
+
+        public abstract void buttonFivePressed();
+        public abstract void buttonSixPressed();
+
+        public void deviceFeedback()
+        {
+            if (deviceState < 0 || deviceState > maxSetting) {
+
+                deviceState = 0;
+
+                Console.WriteLine("On device state: " + deviceState);
+            }
+        }
+
+        public void buttonSevenPressed()
+        {
+            volumeLevel++;
+
+            Console.WriteLine("Volume at: " + volumeLevel);
+        }
+
+        public void buttonEightPressed()
+        {
+            volumeLevel--;
+
+            Console.WriteLine("Volume at: " + volumeLevel);
+        }
 
     }
 }
