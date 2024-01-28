9fbe4ef9a024458aa223d5504599363341c85e5b
Bojan Drezgic <nesquick7777@gmail.com>
2024-01-28 10:19:43 +0100

    Learned Adapter Dessign Pattern

---
+++ OOP/Adapter/EnemyTank.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    internal class EnemyTank : EnemyAttacker
+    {
+        Random random = new Random();
+
+        public void assignDriver(string driverName)
+        {
+            Console.WriteLine(driverName + " is driving the Tank");
+        }
+
+        public void driveForward()
+        {
+            int movement = random.Next(1, 5);
+
+            Console.WriteLine("Enemy Tank moves " + movement + " Spaces");
+        }
+
+        public void fireWeapon()
+        {
+            int attackDamage = random.Next(1,10);
+
+            Console.WriteLine("Enemy Tank deals " + attackDamage + " Damage");
+        }
+    }
+}
---
+++ OOP/Adapter/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Adapter
+{
+    public static class program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            EnemyTank rx7 = new EnemyTank();
+            
+            EnemyRobot fredTheRobot = new EnemyRobot();
+
+            EnemyAttacker robotAdapter = new EnemyRobotAdapter(fredTheRobot);
+
+            Console.WriteLine("Robot");
+
+            fredTheRobot.smashWithHands();
+            fredTheRobot.moveForward();
+            fredTheRobot.reactToHuman("Frank");
+
+            Console.WriteLine("Tank");
+            rx7.fireWeapon();
+            rx7.driveForward();
+            rx7.assignDriver("Frank");
+
+            Console.WriteLine("The Robot with Adapter");
+            robotAdapter.fireWeapon();
+            robotAdapter.driveForward();
+            robotAdapter.assignDriver("Frank");
+        }
+    }
+}
