744125d4fcec3f7d5c7c29a330cb70ca65efc6a3
Bojan Drezgic <nesquick7777@gmail.com>
2024-01-29 17:11:06 +0100

    Learning Facade Dessign Pattern

---
+++ OOP/Facade/SecurityCodeCheck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    internal class SecurityCodeCheck
+    {
+        private int securityCode = 1234;
+
+        public int getSecurityCode()
+        {
+            return securityCode;
+        }
+
+
+
+        public Boolean isCodeCorrect(int secCodeToCheck)
+        {
+            if (secCodeToCheck == getSecurityCode())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ OOP/Facade/WelcomeToBank.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    internal class WelcomeToBank
+    {
+        public WelcomeToBank() {
+            Console.WriteLine("Welcome to ABC bank");
+            Console.WriteLine("We are happy to give you money if we can find it");
+
+        }
+    }
+}
