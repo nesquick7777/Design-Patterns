4e63833f8fef0b83836a6e43503d7c6b999e997f
Bojan Drezgic <nesquick7777@gmail.com>
2024-01-31 12:04:38 +0100

    Learned Memento Design Pattern

---
+++ OOP/Memento/ProjectManagmentSystem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    internal class ProjectManagmentSystem
+    {
+        public Memento Memento { get; set; }
+    }
+}
---
+++ OOP/Memento/SoftwareProject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    internal class SoftwareProject
+    {
+        public String TeamLead { get; set; }
+        public int Cost { get; set; }
+        public int BugCount { get; set; }
+
+        public Memento Store()
+        {
+            return new Memento(TeamLead,BugCount,Cost);
+        }
+
+        public void Restore(Memento memento)
+        {
+            this.TeamLead = memento.TeamLead;
+            this.Cost = memento.Cost;
+            this.BugCount = memento.BugCount;
+
+        }
+
+        public override String ToString() { return TeamLead + ", " + BugCount + ", " + Cost; }
+    }
+}
