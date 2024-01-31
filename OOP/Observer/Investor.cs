0afb7e68c5b6e7a19d3474c08bf041769dbf713b
Bojan Drezgic <nesquick7777@gmail.com>
2024-01-31 12:53:11 +0100

    Codded Observer Design Pattern

---
+++ OOP/Observer/Investor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    internal class Investor : IInvestor
+    {
+        private String Name { get; set; }
+        private CryptoCoin coin;
+
+        public Investor(String name)
+        {
+            this.Name = name;
+        }
+
+
+        public void Update(CryptoCoin stock)
+        {
+            Console.WriteLine("Notified {0} of {1}'s change to  {2:C}", Name,stock.Type,stock.Price);
+        }
+
+        public CryptoCoin Stock { get { return coin; } set { coin = value; } }
+    }
+}
