47978402f61fa8c36ca3a45601ae685fb99851e0
Bojan Drezgic <nesquick7777@gmail.com>
2024-02-03 11:59:49 +0100

    Finished Factory Design Pattern

---
+++ OOP/Factory/EnemyShipFactory.cs
@@ -0,0 +1,41 @@
+﻿namespace Factory
+{
+    public class EnemyShipFactory
+    {
+
+        // This could be used as a static method if we
+        // are willing to give up subclassing it
+
+        public EnemyShip makeEnemyShip(String newShipType)
+        {
+
+            EnemyShip newShip = null;
+
+            if (newShipType.Equals("U"))
+            {
+
+                return new UFOEnemyShip();
+
+            }
+            else
+
+            if (newShipType.Equals("R"))
+            {
+
+                return new RocketEnemyShip();
+
+            }
+            else
+
+            if (newShipType.Equals("B"))
+            {
+
+                return new BigUFOEnemyShip();
+
+            }
+            else return null;
+
+        }
+
+    }
+}
---
+++ OOP/Factory/UFOEnemyShip.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class UFOEnemyShip : EnemyShip
+    {
+
+
+    public UFOEnemyShip()
+    {
+
+        setName("UFO Enemy Ship");
+
+        setDamage(20.0);
+
+    }
+
+}
+}
