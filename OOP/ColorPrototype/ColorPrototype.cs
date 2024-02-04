e56b19b5a3b0625714fbfc22d1ddfb8aae79af11
Bojan Drezgic <nesquick7777@gmail.com>
2024-02-04 13:39:07 +0100

    Coded new example for Prototype Design Pattern

---
+++ OOP/ColorPrototype/ColorPrototype.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorPrototype
+{
+    internal abstract class  ColorPrototype
+    {
+        public abstract ColorPrototype Clone();
+    }
+}
