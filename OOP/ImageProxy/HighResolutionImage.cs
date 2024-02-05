d9f218709bcdce01bb10b8742ae9fc08b51ece85
Bojan Drezgic <nesquick7777@gmail.com>
2024-02-05 17:15:51 +0100

    Coded new example of Proxy Design Pattern

---
+++ OOP/ImageProxy/HighResolutionImage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProxy
+{
+    internal class HighResolutionImage : IImage
+    {
+
+        private string components;
+
+        public HighResolutionImage(String imageURL)
+        {
+            this.DownloadImage(imageURL);
+        }
+
+
+        public void DownloadImage(String imageURL)
+        {
+            Console.WriteLine("Loading...");
+            System.Threading.Thread.Sleep(100);
+            this.components = "Cat Image";
+            Console.WriteLine("Done");
+        }
+
+        public void ShowImage()
+        {
+            Console.WriteLine(components);
+        }
+    }
+}
