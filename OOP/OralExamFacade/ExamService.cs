0aa72d8f717806f06565b01d07c22e69ecf2d825
Bojan Drezgic <nesquick7777@gmail.com>
2024-02-05 11:23:34 +0100

    Coded new example for Design Pattern Facade

---
+++ OOP/OralExamFacade/ExamService.cs
@@ -0,0 +1,13 @@
+﻿namespace OralExamFacade
+{
+    internal class ExamService
+    {
+        private int minimum = 50;
+        private Dictionary<String, int> attendance = new Dictionary<String, int> { { "PR115", 45 }, { "PEK150", 70 }, { "PR120", 90 }, };
+
+        public bool HasEnoughPoints(String id)
+        {
+            return this.minimum <= this.attendance[id];
+        }
+    }
+}
---
+++ OOP/OralExamFacade/LabExercisesService.cs
@@ -0,0 +1,13 @@
+﻿namespace OralExamFacade
+{
+    internal class LabExercisesService
+    {
+        private int minimum = 8;
+        private Dictionary<String, int> attendance = new Dictionary<String, int> { { "PR115", 7 }, { "PEK150", 8 }, { "PR120", 80 }, };
+
+        public bool HasEnoughAttendance(String id)
+        {
+            return this.minimum <= this.attendance[id];
+        }
+    }
+}
