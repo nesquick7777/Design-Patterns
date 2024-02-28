b1194fd84647101d12229a48e1950be4c592267b
Bojan Drezgic <nesquick7777@gmail.com>
2024-02-28 14:55:32 +0100

    Coded null object  design pattern

---
+++ OOP/Null object/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Null_object
+{
+    internal abstract class Employee
+    {
+        public abstract bool IsTimeToPay(DateTime time);
+        public abstract void Pay();
+        public static readonly Employee NULL = new NullEmployee();
+        private class NullEmployee : Employee
+        {
+            public override bool IsTimeToPay(DateTime time)
+            {
+                return false;
+            }
+            public override void Pay()
+            {
+            }
+        }
+    }
+}
---
+++ OOP/Null object/Program.cs
@@ -0,0 +1,18 @@
+﻿using Null_object;
+
+namespace NullObject
+{
+    public static class program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Employee e = DB.GetEmployee("Alice");
+            if (e.IsTimeToPay(new DateTime())) { Console.WriteLine("Payday."); }
+
+        }
+    }
+}
+
+
+
