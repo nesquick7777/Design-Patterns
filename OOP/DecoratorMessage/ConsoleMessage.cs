128d710607f482ef15531a2b44014ae816d764dd
Bojan Drezgic <nesquick7777@gmail.com>
2024-03-11 15:46:04 +0100

    Coded new Decorater Design Pattern

---
+++ OOP/DecoratorMessage/ConsoleMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorMessage
+{
+    internal class ConsoleMessage : IMessage
+    {
+        public String Text { get; private set; }
+        public ConsoleMessage(String text)
+        {
+            this.Text = text;
+        }
+        public void Print()
+        {
+            Console.WriteLine(this.Text);
+        }
+    }
+}
---
+++ OOP/DecoratorMessage/IMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorMessage
+{
+    internal interface IMessage
+    {
+        void Print();
+
+    }
+}
